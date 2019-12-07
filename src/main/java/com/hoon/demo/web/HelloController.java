791dc3ee76d057e1cd74e7471f0a0d7893eff1ea
남궁훈 <kant@sk.com>
2019-12-07 10:40:22 +0900

    [hoon-3]임시로 날짜계산 테스트

--- src/main/java/com/hoon/demo/web/HelloController.java
+++ src/main/java/com/hoon/demo/web/HelloController.java
@@ -1,12 +1,32 @@
 package com.hoon.demo.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Slf4j
 @RestController
 public class HelloController {
+    private static final long ONE_DAY = 24L*60L*60L*1000L;
+
     @GetMapping("/hello")
-    public String hello() {
+    public String hello() throws Exception {
+        String date_s = " 2019-12-03 18:10:00.0";
+        SimpleDateFormat dt = new SimpleDateFormat("yyyyy-MM-dd HH:mm:ss");
+        Date date = dt.parse(date_s);
+
+        long value = getElapsedDaysArrival(date);
+        log.info("value : {}", value);
         return "hello";
+    }
+
+    private long getElapsedDaysArrival(Date arrivedAt) {
+        long calDate = System.currentTimeMillis() - arrivedAt.getTime();
+        long calDateDays = calDate / ONE_DAY;
+        log.info("calDateDays : {}", calDateDays);
+        return Math.abs(calDateDays);
     }
 }
